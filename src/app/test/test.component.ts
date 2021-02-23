e131d148800f7c6283783de77e21c89fb9731990
amirbasheer <amirbashir880@gmail.com>
2021-02-23 12:52:27 +0500

    Angular Tutorial - 4 - Components

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: '<div>Inline template</div>',
+  styles: [`div {
+    color:red,
+  }`]
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
